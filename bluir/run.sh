5d0b0a04054aaa3ddb001fb871854e96819bab65
Rafed Muhammad Yasir <rafed123@gmail.com>
2025-03-10 14:16:10 -0700

    Fix bluid dataset path

--- bluir/run.sh
+++ bluir/run.sh
@@ -2,7 +2,7 @@ projects=("aspectj" "birt" "eclipse" "jdt" "swt" "tomcat")
 type=$1  # baseline, clean
 
 for proj in "${projects[@]}"; do
-  command="python evaluator.py ../../FL-Bluir/results-${type}/BLUiR_${proj}/recommended ../../FL-BLUiR/dataset/${proj}-updated-data.xml > ${type}_${proj}.txt"
+  command="python evaluator.py ../../FL-Bluir/results-${type}/BLUiR_${proj}/recommended ../../FL-Bluir/dataset/${proj}-updated-data.xml > ${type}_${proj}.txt"
   echo "Running ${proj} ${type}: $command"
   eval "$command"
 done
