6bea082883526ee1f1087f47ef6fc80803a724e3
mou23 <bsse0731@iit.du.ac.bd>
2025-06-01 13:24:12 -0700

    similarity score analysis for DreamLoc

--- DreamLoc/similarity_score_processor.py
+++ DreamLoc/similarity_score_processor.py
@@ -10,25 +10,29 @@ index = int(sys.argv[5])
 bug_data = get_bug_data_with_similarity_score(bug_report_file, result_file, index)
 
 def process_similarity_scores():
-    with open(f"{type}_similarity_score_{project_name}.csv", mode='w', newline='') as f:
+    with open(f"similarity_score_{project_name}_{type}.csv", mode='w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(["bug_id", "avg_score", "similarity_score_of_first_correct_file"])
 
         for current_bug_data in bug_data:
             score = 0
             top_k = 10
-            for k, v in list(current_bug_data['suspicious_file_data'].items())[:top_k]:
-                score += v
-            avg_score = score / top_k
+            try:
+                for k, v in list(current_bug_data['suspicious_file_data'].items())[:top_k]:
+                    score += v
+                avg_score = score / top_k
 
-            fixed_files = current_bug_data['files'].split('.java')
-            fixed_files = [(file + '.java').strip() for file in fixed_files[:-1]]
-            similarity_score_of_first_correct_file = None
-            for k, v in current_bug_data['suspicious_file_data'].items():
-                if k in fixed_files:
-                    similarity_score_of_first_correct_file = v
-                    break
+                fixed_files = current_bug_data['files'].split('.java')
+                fixed_files = [(file + '.java').strip() for file in fixed_files[:-1]]
+                similarity_score_of_first_correct_file = None
+                for k, v in current_bug_data['suspicious_file_data'].items():
+                    if k in fixed_files:
+                        similarity_score_of_first_correct_file = v
+                        break
 
-            writer.writerow([current_bug_data["bug_id"], avg_score, similarity_score_of_first_correct_file])
+                writer.writerow([current_bug_data["bug_id"], avg_score, similarity_score_of_first_correct_file])
+            except Exception as Error:
+                print(f"Error processing bug {current_bug_data['bug_id']}: {str(Error)}")
+                continue
 
 process_similarity_scores()
